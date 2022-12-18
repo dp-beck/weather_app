a9adc686adbc1f817aa1a3e1ed4c7195ee87afaa
Dan Beck <beckdp01@gmail.com>
2022-12-18 09:08:11 -0500

    fix call to unsecured http

--- src/findCoordinates.js
+++ src/findCoordinates.js
@@ -3,7 +3,7 @@
 
 async function findCoordinates(cityName, stateCode, countryCode) {
   try {
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=8696ac5681360fb97515b98c3d94684c`, { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=8696ac5681360fb97515b98c3d94684c`, { mode: 'cors' });
     const data = await response.json();
     const coordinates = [data[0].lat, data[0].lon];
     return coordinates;
